f2689e6cb7ed1de26f6cbfa188000a2d44cd5e75
Mark Goetz <breakawayblue@gmail.com>
2018-02-03 16:51:37 -0600

    Adds vertical layout test case

---
+++ src/components/VerticalLayout/index.test.js
@@ -0,0 +1,15 @@
+import { h } from 'preact';
+import VerticalLayout from './index';
+import { shallow } from 'preact-render-spy';
+
+describe('Vertical Layout', () => {
+
+  test('Vertical layout renders correctly', () => {
+    const context = shallow(<VerticalLayout>
+      <div>Test element</div>
+      <button>Test button</button>
+      <div>Extra test element</div>
+    </VerticalLayout>);
+    expect(context).toMatchSnapshot();
+  });
+});
