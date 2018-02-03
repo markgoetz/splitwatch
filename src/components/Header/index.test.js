556d62e24c2d1ec581852755a8411f15ea893430
Mark Goetz <breakawayblue@gmail.com>
2018-02-03 15:55:20 -0600

    Sets up snapshot testing

--- src/components/Header/index.test.js
+++ src/components/Header/index.test.js
@@ -1,14 +1,11 @@
 import { h } from 'preact';
 import Header from './index';
-import { Link } from 'preact-router/match';
-// See: https://github.com/mzgoddard/preact-render-spy
 import { shallow } from 'preact-render-spy';
 
 describe('Header Component', () => {
 
-  test('Header renders 3 nav items', () => {
+  test('Header renders correctly', () => {
     const context = shallow(<Header />);
-    expect(context.find('h1').text()).toBe('Preact App');
-    expect(context.find(<Link />).length).toBe(3);
+    expect(context).toMatchSnapshot();
   });
 });
