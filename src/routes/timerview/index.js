440059595fb656d573cecec45889ad3f70e342cd
Mark Goetz <breakawayblue@gmail.com>
2018-02-10 16:48:47 -0600

    Adds split list to timer page

--- src/routes/timerview/index.js
+++ src/routes/timerview/index.js
@@ -1,10 +1,12 @@
 import { h } from 'preact';
 import TimerComponent from '../../components/TimerComponent';
+import SplitList from '../../components/SplitList';
 
 export default class TimerView {
   render() {
     return (<div>
       <TimerComponent {...this.props} />
+      <SplitList splits={this.props.splits} />
     </div>);
   }
 }
