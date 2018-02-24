d054ee0c392e71234825f1d3d45cae6d7330c378
Mark Goetz <breakawayblue@gmail.com>
2018-02-24 13:04:21 -0600

    Additional unit tests

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,7 +3,7 @@ import style from './style.css';
 
 export default function Button(props) {
   const baseClassName = style.button;
-  const variantClassName = props.isPrimary ? style.buttonPrimary : style.buttonSecondary;
+  const variantClassName = props.isPrimary ? style.primary : style.secondary;
   const className = [baseClassName, variantClassName].join(' ');
   return <button class={className} onClick={this.props.onClick}>{this.props.children}</button>;
 }
